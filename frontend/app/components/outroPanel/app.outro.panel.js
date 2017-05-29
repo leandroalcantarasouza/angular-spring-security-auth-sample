a3bf3cc6c911c229766cf3e8898f742fc0d6044e
Leandro Souza <leandro.alcantara.souza@gmail.com>
2017-05-29 16:57:19 -0300

    initial commit

--- frontend/app/components/outroPanel/app.outro.panel.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-
-  angular.module('app').component('appOutroPanel',{
-    template:'<div ng-show="$root.authenticated" class="jumbotron"><p class="text-center text-primary">Soh auth!</p></div>'
-  });
-
-})();
